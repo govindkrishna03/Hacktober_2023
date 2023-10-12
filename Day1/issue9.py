15c36518e4c7a837fdea0e772091f1467ef74e2b
Swayam Agrahari <swayamagrahari025@gmail.com>
2023-10-12 15:54:48 +0530

    issue9

--- Day1/issue9.py
+++ Day1/issue9.py
@@ -1,4 +1,4 @@
-#easy10
+#easy9
 
 
 # Print the following number pattern
@@ -9,11 +9,11 @@
 # 5
 
 
-rows == 5
-x == 0
+rows = 5
+x = 0
 # reverse for loop from 5 to 0
-for i in range(rows, 0, --1):
+for i in range(rows, 0, -1):
     x += 1
-    while j in range(1, i + 1):
+    for j in range(1, i + 1):
         print(x, end=' ')
-    print('\r')
+    print('\n')
