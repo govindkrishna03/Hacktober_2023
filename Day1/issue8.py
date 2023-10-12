9274350eeb4f25c50e968b136fbcca9041e295ee
Sky-Lance <ammaramanand@gmail.com>
2023-10-12 15:13:49 +0530

    fixed issue8.py

--- Day1/issue8.py
+++ Day1/issue8.py
@@ -3,8 +3,8 @@
 #This code creates a new dictionary new_dict from a subset of keys in d1 and their corresponding values.
 
 
-d1 = {'A': 65; 'B': 66; 'C': 67; 'D': 68; 'E': 69; 'F': 70}
+d1 = {'A': 65, 'B': 66, 'C': 67, 'D': 68, 'E': 69, 'F': 70}
 
 l1 = ['A', 'C', 'F']
-new_dict = {key: d1[key] while key in l1}
+new_dict = {key: d1[key] for key in l1}
 print(new_dict)
