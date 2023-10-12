0a36ff6948003a350926d1d1cfd9b2a9e7703aac
Sivabharathi <vssivabharathi@gmail.com>
2023-10-13 02:24:26 +0530

    Fixed problem6

--- Day1/issue6.py
+++ Day1/issue6.py
@@ -3,9 +3,9 @@
 
 
 sum = 0
-number = input("Enter an integer: ")
+number = int(input("Enter an integer: "))
 while number!=0:
-    digit = number%10
-    sum += sum+digit
-    number = number//10
+      digit = number%10
+      sum += sum+digit
+      number = number//10
 print("Sum of digits is: ", sum)
