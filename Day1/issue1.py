c6d5a2ddbc42ddca4d2eed6cbd27d070efd45754
Sivabharathi <vssivabharathi@gmail.com>
2023-10-12 21:22:34 +0530

    fixed 1

--- Day1/issue1.py
+++ Day1/issue1.py
@@ -4,9 +4,8 @@
 print("Printing current and previous number and their sum in a range(10)")
 previous_num = 0
 
-
-for i in range(1, 11)
+for i in range(1, 11):
     x_sum = previous_num + i
-    print("Current Number", i, "Previous Number ", previous num, " Sum: ", x_sum)
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
     previous_num = i
 
