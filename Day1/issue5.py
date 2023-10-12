a81eea0a5a13c70d05904b6ee2ff83ef1c0887c9
Riduvarshini <riduvarshini@riduvarshinirs6@gmail.com>
2023-10-12 15:32:01 +0530

    done

--- Day1/issue5.py
+++ Day1/issue5.py
@@ -6,9 +6,9 @@
 
 num = input("Enter your number: ")
 count = 0
-while num = 0:
+while num == 0:
  
-    num == num // 10
+    num =num // 10
 
     count = count + 1
 print("Total digits are:", count)
