2408a8f95b301d9013cf1abe49ed73936bdb6f45
sabarixr <sabarinath34215@gmail.com>
2023-10-12 19:03:46 +0530

    fixed6

--- Day1/issue6.py
+++ Day1/issue6.py
@@ -2,10 +2,10 @@
 #Code calculates the sum of the digits of a user-input integer.
 
 
-sum == 0
-number = input("Enter an integer: ")
-while(number!=0)
+sum = 0
+number = int(input("Enter an integer: "))
+while number!=0:
     digit = number%10
-    sum +-= sum+digit
+    sum = sum+digit
     number = number//10
 print("Sum of digits is: ", sum)
