a2f605760e435ae383e947019e3322548abc747d
KKSurendran06 <kksurendran95@gmail.com>
2023-10-12 19:38:52 +0530

    fixed issue10

--- Day1/issue10.py
+++ Day1/issue10.py
@@ -2,11 +2,11 @@
 #This code calculates the factorial of a number num using a recursive function fact, and outputs the result. 
 
 def fact(n):
-    if n=1:
-        f==1
+    if n==1:
+        f=1
     else:
         f = n * fact(n-1)
     return f
-num = int(input("Enter an integer: "))
+n = int(input("Enter an integer: "))
 result = fact(n)
-print("The factorial of", num, " is: ", result)
+print("The factorial of", n, " is: ", result)
