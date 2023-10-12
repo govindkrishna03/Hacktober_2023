f7fc7e7952ebd1cdfd99e8e7f26a25cdb6216eee
Sivabharathi <vssivabharathi@gmail.com>
2023-10-13 00:58:07 +0530

    Fixed problem4

--- Day1/issue4.py
+++ Day1/issue4.py
@@ -5,8 +5,8 @@ c = 0
 p = 1.0
 count = int(input("Enter the number of values: "))
 while(c<count):
-x = float(input("Enter a real number: "))
-    c = c+1
-    p = p * x
+     x = float(input("Enter a real number: "))
+     c = c+1
+     p = p * x
 gm = pow(p,1.0/count)
 print("The geometric mean is: ",gm)
