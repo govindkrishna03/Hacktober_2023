164fe1935794e701cbd5daf32b0eb2a6cf14d06d
Sky-Lance <ammaramanand@gmail.com>
2023-10-12 14:37:27 +0530

    fixed issue1.py

--- Day1/issue1.py
+++ Day1/issue1.py
@@ -7,6 +7,6 @@ previous_num = 0
 
 for i in range(1, 11)
     x_sum = previous_num + i
-    print("Current Number", i, "Previous Number ", previous num, " Sum: ", x_sum)
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
     previous_num = i
 
