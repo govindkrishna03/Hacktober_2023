67f07690a089265732104db38e450daf33f7babe
Swayam Agrahari <swayamagrahari025@gmail.com>
2023-10-12 15:48:00 +0530

    issue7

--- Day1/issue7.py
+++ Day1/issue7.py
@@ -1,10 +1,16 @@
 #easy8
 #This code finds the largest element and its position in the list of numbers [3,8,1,7,2,9,5,4] and prints the result.
-numbers = [3,8,1,7,2,9,5,4]
-big = numbers[0]:
+
+# This code finds the largest element and its position in the list of numbers [3,8,1,7,2,9,5,4] and prints the result.
+
+numbers = [3, 8, 1, 7, 2, 9, 5, 4]
+big = numbers[0]
 position = 0
-for i in range(len(numbers))):
+
+for i in range(1, len(numbers)):
     if (numbers[i]>big):
         big = numbers[i]
-        position = j
-((print("The largest element is ",big," which is found at position ",position)))
+        position = i
+
+print("The largest element is", big, "which is found at position", position)
+
