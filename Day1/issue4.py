7e2f77c08a26a8bffe673990b88dc89871cd1814
Swayam Agrahari <swayamagrahari025@gmail.com>
2023-10-12 15:10:32 +0530

    issue4

--- Day1/issue4.py
+++ Day1/issue4.py
@@ -1,12 +1,15 @@
 #easy5
-# program to find the geometric mean of n numbers 
+# Program to find the geometric mean of n numbers
 
 c = 0
 p = 1.0
 count = int(input("Enter the number of values: "))
-while(c<count):
-x = float(input("Enter a real number: "))
-    c = c+1
+
+while c < count:
+    x = float(input("Enter a real number: "))
+    c = c + 1
     p = p * x
-gm = pow(p,1.0/count)
-print("The geometric mean is: ",gm)
+
+gm = pow(p, 1.0 / count)
+print("The geometric mean is:", gm)
+
