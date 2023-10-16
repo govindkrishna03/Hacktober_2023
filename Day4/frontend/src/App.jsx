b9fa24b39c9a914ad35b55ece9332536d6e0775d
sabarixr <sabarinath34215@gmail.com>
2023-10-17 00:28:45 +0530

    day 3 is half wrong day4 first commit

--- Day4/frontend/src/App.jsx
+++ Day4/frontend/src/App.jsx
@@ -7,33 +7,42 @@ import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import { useSelector } from "react-redux";
-import Donate from './pages/donate'
+import Donate from "./pages/donate";
 import Receive from "./pages/recieve";
 import Registration from "./pages/auth/regForm";
 
 function App() {
-  const { access_token } = useSelector(state => state.auth)
+  const { access_token } = useSelector((state) => state.auth);
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-          <Route index element={<Contact />} />
-          <Route path="home" element={<Home />} />
-          {access_token ? (
-          <>
-          <Route path="Receive" element={<Receive />} />
-          </>
-          ) : null}
-          <Route path="login" element={!access_token ? <LoginReg /> : <Navigate to="/" />} />
-          <Route path="/Register" element={<LoginReg />} /> 
-          <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
-          <Route path="api/user/reset/:id/:token" element={<ResetPassword />} />
+            <Route index element={<Contact />} />
+            <Route path="home" element={<Home />} />
+            {access_token ? (
+              <Route path="Receive" element={<Receive />} />
+            ) : null}
+            <Route
+              path="login"
+              element={!access_token ? <LoginReg /> : <Navigate to="/" />}
+            />
+            <Route path="/Register" element={<LoginReg />} />
+            <Route
+              path="sendpasswordresetemail"
+              element={<SendPasswordResetEmail />}
+            />
+            <Route
+              path="api/user/reset/:id/:token"
+              element={<ResetPassword />}
+            />
+            <Route
+              path="dashboard"
+              element={access_token ? <Dashboard /> : <Navigate to="/login" />}
+            />
           </Route>
-          <Route path="/dashboard" element={1 ? <Dashboard /> : <Navigate to="/login" />} />
           <Route path="*" element={<h1>Please Login First!</h1>} />
         </Routes>
-
       </BrowserRouter>
     </>
   );
